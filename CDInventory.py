d962f44bd024dfca6c0107fbce5e16eb45cdf281
Rupa Guha <guhasri@hotmail.com>
2020-08-31 21:42:43 -0700

    Added further methods
    Added further methods

--- CDInventory.py
+++ CDInventory.py
@@ -2,8 +2,9 @@
 # Title: CDInventory.py
 # Desc: Assignnment 08 - Working with classes
 # Change Log: (Who, When, What)
-# Rupa Guha, 2020-August-01, created file
-# Rupa Guha, 2020-August-01, added code to create the CD class
+# Rupa Guha, 2020-August-31, created file
+# Rupa Guha, 2020-August-31, added code to create the CD class
+# Rupa Guha, 2020-August-31, added code for some of the methods in FileIO and IO classes as well as main body 
 #------------------------------------------#
 
 # -- DATA -- #
@@ -21,10 +22,10 @@ class CD:
 
     """
     # TODone Add Code to the CD class
-    #----Constructor---------
+    #----Constructor--------
     
     def __init__(self,cd_id,cd_title,cd_artist):
-         #--------Attributes
+         #-----Attributes-----
         self.cd_id = cd_id
         self.cd_title = cd_title
         self.cd_artist = cd_artist
@@ -68,9 +69,9 @@ class FileIO:
         counter = 0
 
         while counter < (len(lst_Inventory) -1):
-            lst_Inventory[0].cd_id = new_line + lst_Inventory[0].cd_id + ","
-            lst_Inventory[0].cd_title = new_line + lst_Inventory[0].cd_title + ","
-            lst_Inventory[0].cd_artist = new_line + lst_Inventory[0].cd_artist + ","
+            new_line = new_line + lst_Inventory[counter].cd_id + ","
+            new_line = new_line + lst_Inventory[counter].cd_title + ","
+            new_line = new_line + lst_Inventory[counter].cd_artist + ","
             new_line = new_line[:-1] + "\n"
             counter += 1
 
@@ -85,7 +86,7 @@ class FileIO:
 class IO:
     # TODO add docstring
     
-    # TODO add code to show menu to user
+    # TODone add code to show menu to user
     @staticmethod
     def print_menu():
         """Displays a menu of choices to the user
@@ -98,9 +99,9 @@ class IO:
         """
 
         print('Menu\n\n[l] load Inventory from file\n[a] Add CD\n[i] Display Current Inventory')
-        print('[d] delete CD from Inventory\n[s] Save Inventory to file\n[x] exit\n')
+        print('[s] Save Inventory to file\n[x] exit\n')
 
-    # TODO add code to captures user's choice
+    # TODone add code to captures user's choice
     @staticmethod
     def menu_choice():
         """Gets user input for menu selection
@@ -109,16 +110,20 @@ class IO:
             None.
 
         Returns:
-            choice (string): a lower case sting of the users input out of the choices l, a, i, d, s or x
+            choice (string): a lower case sting of the users input out of the choices l, a, i, s or x
 
         """
         choice = ' '
-        while choice not in ['l', 'a', 'i', 'd', 's', 'x']:
-            choice = input('Which operation would you like to perform? [l, a, i, d, s or x]: ').lower().strip()
+        while choice not in ['l', 'a', 'i', 's', 'x']:
+            choice = input('Which operation would you like to perform? [l, a, i, s or x]: ').lower().strip()
         print()  # Add extra space for layout
         return choice
     
     # TODO add code to display the current data on screen
+    def show_inventory(lstObj):
+        
+        pass
+    
     # TODO add code to get CD data from user
     pass
 
@@ -127,11 +132,40 @@ class IO:
 # Load data from file into a list of CD objects on script start
 FileIO.load_inventory(strFileName, lstOfCDObjects)
 
-# Display menu to user
-    # show user current inventory
-    # let user add data to the inventory
-    # let user save inventory to file
-    # let user load inventory from file
+while True:
+    # Display menu to user
+    IO.print_menu()
+    strChoice = IO.menu_choice()
+    
     # let user exit program
+    if strChoice == 'x':
+        break
+    
+    # let user load inventory from file
+    if strChoice == 'l':
+        print('WARNING: If you continue, all unsaved data will be lost and the Inventory re-loaded from file.')
+        strYesNo = input('type \'yes\' to continue and reload from file. otherwise reload will be canceled')
+        if strYesNo.lower() == 'yes':
+            print('reloading...')
+            FileIO.read_inventory(strFileName, lstOfCDObjects)
+            IO.show_inventory(lstOfCDObjects)
+        else:
+            input('canceling... Inventory data NOT reloaded. Press [ENTER] to continue to the menu.')
+            IO.show_inventory(lstOfCDObjects)
+        continue  # start loop back at top.
+    
+    # let user add data to the inventory
+    if strChoice == 'a':
+        pass
+    
+    # show user current inventory
+    if strChoice == 'i':
+        pass
+
+    # let user save inventory to file
+    if strChoice == 's':
+        FileIO.save_inventory(strFileName, lstOfCDObjects)
+        
+    
     
 
